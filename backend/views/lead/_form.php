ce81f017a449733d49fa5f83e7f625f0c2a35108
yii2nasir <nasir@dhwaniris.com>
2019-03-29 23:09:45 +0530

    update
    update

--- backend/views/lead/_form.php
+++ backend/views/lead/_form.php
@@ -2,6 +2,7 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use kartik\datetime\DateTimePicker;
 
 /* @var $this yii\web\View */
 /* @var $model backend\models\Lead */
@@ -34,15 +35,26 @@ if($model->isNewRecord == false){
 if($model->isNewRecord == false){
     //echo $form->field($model, 'last_call_time')->textInput();
     echo $form->field($model, 'last_feedback')->textarea(['maxlength' => true])->label('Feed back');
-    echo $form->field($model, 'next_call_time')->textInput();
+   // echo $form->field($model, 'next_call_time')->textInput(['maxlength' => true,"class" => "form-control datePickerTodayMax",'required'=>true]);
+   echo '<label>Next Shedule Date/Time</label>';
+echo DateTimePicker::widget([
+    'name' => 'Lead[next_call_time]',
+    'options' => ['placeholder' => 'Select operating time ...'],
+    'convertFormat' => true,
+    'pluginOptions' => [
+        //'format' => 'd-M-Y g:i A',
+        'format' => 'Y-m-d g:i',
+        //'startDate' => '15-Mar-2019 12:00 AM',
+        'todayHighlight' => true
+    ]
+]);
 }
 
 ?>
 
+
     <?php // $form->field($model, 'last_call_time')->textInput() ?>
-
     <?php // $form->field($model, 'last_feedback')->textInput(['maxlength' => true]) ?>
-
     <?php // $form->field($model, 'next_call_time')->textInput() ?>
 
     <div class="form-group">
