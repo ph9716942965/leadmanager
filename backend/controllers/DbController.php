e969994da864d45bb6dc3393606a98af36cc5a18
yii2nasir <nasir@dhwaniris.com>
2019-04-06 02:32:14 +0530

    mignew
    mignew

--- backend/controllers/DbController.php
+++ backend/controllers/DbController.php
@@ -32,6 +32,45 @@ class DbController extends Controller
         ];
     }
 
+    public function actionMigrate(){
+        $tbl[]='CREATE TABLE if not exists `db` (
+            `id` smallint(6) NOT NULL AUTO_INCREMENT,
+            `name` varchar(50) NOT NULL,
+            `email` varchar(100) NOT NULL,
+            `whatsapp` varchar(15) NOT NULL,
+            `phone` varchar(15) NOT NULL,
+            `address` varchar(250) NOT NULL,
+            `create_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+            PRIMARY KEY (`id`)
+           ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1';
+        $tbl[]='CREATE TABLE if not exists `appointment` (
+            `id` int(11) NOT NULL AUTO_INCREMENT,
+            `db_id` smallint(6) NOT NULL,
+            `appointment_date_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+            `create_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+            PRIMARY KEY (`id`),
+            KEY `db_id` (`db_id`),
+            CONSTRAINT `appointment_ibfk_1` FOREIGN KEY (`db_id`) REFERENCES `db` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+           ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1
+           ';
+        $tbl[]='CREATE TABLE if not exists `call_log` (
+            `id` int(11) NOT NULL AUTO_INCREMENT,
+            `db_id` smallint(6) NOT NULL,
+            `review` varchar(250) DEFAULT NULL,
+            `create_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+            PRIMARY KEY (`id`),
+            KEY `db_id` (`db_id`),
+            CONSTRAINT `call_log_ibfk_1` FOREIGN KEY (`db_id`) REFERENCES `db` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+           ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1';
+       // $tbl[]='';
+        foreach($tbl as $migrate_tbl){
+            $connection = Yii::$app->getDb();
+            $command = $connection->createCommand($migrate_tbl);
+            $res=$command->execute();
+            $res=$command->queryAll();
+        }
+    }
+
     /**
      * Lists all Db models.
      * @return mixed
